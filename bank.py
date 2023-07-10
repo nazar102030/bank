0d69a551130a6f2fade6d8937af2f435e71cc9fb
Nazar <nazarown777@gmail.com>
2023-07-10 11:28:45 +0500

    first commit

---
+++ bank.py
@@ -0,0 +1,19 @@
+class Bank():
+    def __init__(self, num):
+        self.num = num
+        num = 0
+
+    def credit(num):
+        if num >= 1000:
+            print(num % 2)
+        elif num >= 10000 or num >= 100000 or num >= 1000000:
+            print(num % 20)
+
+    def epotec(num):
+        if num >= 1000:
+            print(num % 30)
+        elif num >= 10000 or num >= 100000 or num >= 1000000:
+            print(num % 40 or num % 60)
+
+    
+ 
